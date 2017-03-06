c71de115487b08fad7cfb0c5d79821c1864ae5bc
fenixscd <fenixscd@gmail.com>
2017-03-06 19:55:03 +0100

    Ejercicio con jquery

---
+++ Ejercicio-25/js/app.js
@@ -0,0 +1,29 @@
+//alert("Hola caracola ...");
+//console.log("Hola caracola ....");
+
+$(document).ready(function($){
+    $( "#target" ).click(function() {
+        alert( "Handler for .click() called." );
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
